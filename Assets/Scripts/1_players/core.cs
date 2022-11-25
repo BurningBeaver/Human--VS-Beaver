d5a0bde13da014d688cb1d7b4a1ce12215f88240
redflag-HSH <66863292+redflag-HSH@users.noreply.github.com>
2022-11-26 00:18:07 +0900

    player core
    core first prototype

---
+++ Assets/Scripts/1_players/core.cs
@@ -0,0 +1,71 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class sameMove : MonoBehaviour
+{
+    //[SerializeField]
+    //GameObject interactingObject;
+    public KeyCode interAct, up, down, left, right;
+    [SerializeField]
+    float mSpeed;
+    int keyItemCount, gageCount;
+    bool over;
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+    public void MoveCheck()
+    {
+        float x = 0, y = 0;
+        if (Input.GetKey(up))
+            y = 1;
+        if (Input.GetKey(down))
+            y = -1;
+
+        if (Input.GetKey(left))
+            x = -1;
+        if (Input.GetKey(right))
+            x = 1;
+        if (!over)
+            transform.Translate(new Vector2(x * mSpeed, y * mSpeed));
+    }
+
+    public void InterActCheck()
+    {
+        if (Input.GetKeyDown(interAct))
+        {
+            //return this.transform.position;
+        }
+    }
+    public void GameOver()
+    {
+        over = true;
+    }
+    /* public void InteractStart(GameObject inter)
+     {
+         interactingObject = inter;
+         InteractGageUp();
+     }
+     public void InteractGageUp()
+     {
+         gageCount++;
+         if (gageCount >= 10)
+         {
+             gageCount = 0;
+             interactingObject = null;
+             InteractEnd();
+         }
+     }
+     public virtual void InteractEnd()
+     {
+
+     }*/
+}
