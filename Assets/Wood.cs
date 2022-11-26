8349802c97a1bc99e7cb247621820fc2a7df2720
rrrmaster <dudgus9101@gmail.com>
2022-11-26 22:33:28 +0900

    ads

--- Assets/Wood.cs
+++ Assets/Wood.cs
@@ -6,12 +6,20 @@ using UnityEngine;
 
 public class Wood : MonoBehaviour
 {
-    private void Awake()
+    public float timer;
+    private IEnumerator Start()
     {
         Sequence a = DOTween.Sequence();
         a.Append(transform.DOMoveY(0.5f, 0.5f).SetRelative());
         a.Join(transform.DOScale(1, 0.5f).From(0));
         a.Join(transform.GetComponent<SpriteRenderer>().DOFade(1, 0.5f).From(0));
         a.Play();
+
+        yield return new WaitForSeconds(timer);
+        transform.DOScale(0, 0.5f).OnComplete(() =>
+        {
+            Destroy(gameObject);
+        });
     }
+
 }
